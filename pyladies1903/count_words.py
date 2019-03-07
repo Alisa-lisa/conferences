17305d4102f7fc615bd29ead48d97b10f07f3ba2
alisa <alisa.dammer@gmail.com>
2019-03-07 11:41:57 +0100

    small adjustments

--- pyladies1903/count_words.py
+++ pyladies1903/count_words.py
@@ -1,20 +1,23 @@
 from pyladies_wordcount import *
 import re
 
-def simple_count():
+
+def simple_count(file):
     res = 0
-    with open('lol.txt') as f:
+    with open(file) as f:
         for line in f:
             if re.findall(r"\bAlice|ALICE\b", line):
                 res += 1
     return res
 
-def fast_count():
-    return sum(1 for match in re.finditer(r"\bAlice|ALICE\b", open("lol.txt").read()))
 
-def magic_count(simple=True):
+def fast_count(file):
+    return sum(1 for match in re.finditer(r"\bAlice|ALICE\b", open(file).read()))
+
+
+def magic_count(file, simple=True):
     if simple:
-        return WordCounter("lol.txt").search_sequential("Alice")
+        return WordCounter(file).search_sequential("Alice")
     else:
-        return WordCounter("lol.txt").search("Alice")
+        return WordCounter(file).search("Alice")
 
--- pyladies1903/main.py
+++ pyladies1903/main.py
@@ -2,12 +2,13 @@ import sys
 from count_words import fast_count, simple_count, magic_count
 
 if __name__ == '__main__':
-    command = sys.argv[1]
+    command = sys.argv[2]
+    file = sys.argv[1]
     if command == "smart":
-        print(fast_count())
+        print(fast_count(file))
     elif command == "seq":
-        print(magic_count(True))
+        print(magic_count(file, True))
     elif command == "rayon":
-        print(magic_count(False))
+        print(magic_count(file, False))
     else:
-        print(simple_count())
+        print(simple_count(file))
