27ea5780858da644b5d97c9781dfd13ec2ff6818
Alisa Dammer <alisa.dammer@gmail.com>
2019-06-23 16:41:26 +0200

    finish bdbbq presentation

--- BigDataBBQ19/tpot_exported_pipeline.py
+++ BigDataBBQ19/tpot_exported_pipeline.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
+from sklearn.feature_selection import RFE
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import Binarizer
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +12,12 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Average CV score on the training set was:0.8272743784497807
-exported_pipeline = LogisticRegression(C=25.0, dual=False, penalty="l1")
+# Average CV score on the training set was:0.8091091137598685
+exported_pipeline = make_pipeline(
+    RFE(estimator=ExtraTreesClassifier(criterion="entropy", max_features=0.35000000000000003, n_estimators=100), step=0.25),
+    Binarizer(threshold=0.45),
+    RandomForestClassifier(bootstrap=True, criterion="entropy", max_features=0.8, min_samples_leaf=16, min_samples_split=13, n_estimators=100)
+)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
