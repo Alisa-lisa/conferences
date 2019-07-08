54cab5420a4693779ca36b6b658037e511ab7a9f
a.dammer <a.dammer@mytaxi.com>
2019-07-08 22:19:31 +0200

    finished rust main loop

--- EP_2019/rust_impl/src/core.rs
+++ EP_2019/rust_impl/src/core.rs
@@ -1,12 +1,13 @@
 /// main module binding everyting together
 use std::collections::HashMap;
+use rand::prelude::*;
 use uuid::Uuid;
+use chrono::{DateTime, NaiveDateTime, FixedOffset};
 
 use crate::car;
 use crate::passenger;
 use crate::request;
 
-#[derive(Default)]
 pub struct Clock {
     /// Clock is responsible for the timeline of the simulation
     ///
@@ -16,38 +17,32 @@ pub struct Clock {
     /// - start: chrono::DateTime start of the simulation time line
     /// - end: chrono::DateTime end of the simulation time line
     /// - now: u64 index of the current step on the timeline
-    /// - number_ticks: u64 total number of steps the simulation will be running 
+    /// - number_ticks: u64 total number of steps the simulation will be running
     pub step: u16,
     pub unit: String,
-    pub start: String,
-    pub end: String,
-    //pub start: DateTime<FixedOffset>,
-    //pub end: DateTime<Utc>,     
+    // pub start: String,
+    //pub end: String,
+    pub start: DateTime<FixedOffset>,
+    pub end: DateTime<FixedOffset>,
     pub now: u64,
     pub number_ticks: u64,
 }
 
 impl Clock {
-/*    pub fn create(&mut self, start: String, end: String) -> Clock {
-        /// Proper Clock creation withh all values set
-        ///
-        /// Parameters:
-        /// - start: String date time in RFC 3339 format
-        /// - end: String date time in RFC 3339 format
-        ///
-        /// Return: Clock
-        let s = DateTime::parse_from_rfc3339(&start);
-        let e = DateTime::parse_from_rfc3339(&end);
-        let delta = (e - s).clone();
-        println!("{}", delta.second());
-        let clock = Clock{step: 1, unit: "s".to_string(), start: s, end: e, now: 0, number_ticks: 0};
+    /// Proper Clock creation withh all values set
+    ///
+    /// Parameters:
+    /// - start: String date time in RFC 3339 format
+    /// - end: String date time in RFC 3339 format
+    ///
+    /// Return: Clock
+    pub fn new(start: &str, end: &str) -> Clock {
+        let s = DateTime::parse_from_rfc3339(&start).expect("Parsing went wrong");
+        let e = DateTime::parse_from_rfc3339(&end).expect("Parsing went wrong");
+        let delta = (e - s);
+        let clock = Clock{step: 1, unit: "s".to_string(), start: s, end: e, now: 0, number_ticks: delta.num_seconds() as u64};
         clock
     }
-*/
-    pub fn get_steps(&mut self) -> u64 {
-        // Count total number ticks 
-        120
-    } 
 
     pub fn tick(&mut self) {
         // Move step further on the timeline
@@ -64,7 +59,7 @@ impl Clock {
         //
         // Return date time string in RFC_3399 format
         format!("Tick {} is {}", self.now, "TODO")
-    } 
+    }
 }
 
 pub struct World {
@@ -78,7 +73,7 @@ pub struct World {
     /// - passenger: HashMap<u32, Passenger> all passenger present in the simulation
     /// - requests: HashMap<uuid, Request> all requests created by the passengers
     pub clock: Clock,
-    pub x: u32, 
+    pub x: u32,
     pub y: u32,
     pub cars: HashMap<u32, car::Car>,
     pub passengers: HashMap<u32, passenger::Passenger>,
@@ -96,14 +91,83 @@ impl World {
         self.passengers.insert(passenger.id.clone(), passenger);
     }
 
-    pub fn run(&mut self) {
+    pub fn run(&mut self, log: bool) {
+        let mut rng = SmallRng::from_rng(thread_rng()).unwrap();
+        // helper structures to operate on ids similar to python way
+        let mut free_cars = Vec::new();
+        for (i, car) in self.cars.iter() {
+            free_cars.push(i.clone());
+        }
+        //let mut ocuppied_cars = Vec::new();
+        let mut pending = Vec::new();
+        let mut progress = Vec::new();
+        //let mut finished = Vec::new();
         // Main loop of the simulation
-        println!("all ticks {} ", self.clock.number_ticks);
         while !self.clock.is_last_tick() {
-            println!("{}", "1");
             // spawn requests from free users
-            println!("{}", self.clock.now);
-            
+            for (p_id, ref mut p) in self.passengers.iter_mut() {
+                if !p.awaiting {
+                    let request = p.update(&mut rng, 100, 100);
+                    if request.is_some() {
+                        let req = request.unwrap();
+                        let id = req.id.clone();
+                        self.requests.insert(id.clone(), req);
+                        pending.push(id.clone());
+                        if log {
+                            println!("At {} request {} was created by passenger {}", self.clock.now, id.clone(), p_id);
+                        }
+                    }
+                }
+            }
+
+            // start assigning requests to cars
+            for r_id in &pending {
+                // try to assign a request
+                if free_cars.len() > 0 {
+                    let car_id = free_cars.pop();
+                    let print_id = car_id.clone().unwrap();
+                    self.cars.entry(print_id.clone()).and_modify(|c| c.free = false);
+                    self.requests.entry(*r_id).and_modify(|r| r.progress = true);
+                    self.requests.entry(*r_id).and_modify(|r| r.car_id = car_id);
+                    progress.push(r_id.clone());
+                    if log {
+                        println!("At {} Request {} was assigned to a car {}", self.clock.now, r_id.clone(), print_id);
+                    }
+                }
+                // if no cars available reduce lifetime
+                else {
+                    let req = self.requests.get(r_id).unwrap().lifetime.clone();
+                    if req > 0 {
+                        let tick = self.clock.step.clone() as u64;
+                        self.requests.entry(*r_id).and_modify(|r| r.lifetime -= tick);
+                    }
+                    // cancell
+                    else {
+                        self.requests.entry(*r_id).and_modify(|r| r.finished = true);
+                        let p = self.requests.get(r_id).unwrap().passenger.clone();
+                        self.passengers.entry(p).and_modify(|p| p.awaiting = false);
+                        if log {
+                            println!("At {} Request {} was cancelled", self.clock.now, r_id);
+                        }
+                    }
+                }
+            }
+            // start progress on pending and assigned requests
+            for (r_id, mut req) in self.requests.iter_mut() {
+                if req.progress {
+                    // reduce execution time or stop request execution
+                    if req.execution_time <= 0 {
+                        req.finished = true;
+                        self.cars.entry(req.car_id.clone().unwrap()).and_modify(|c| c.free = true);
+                        if log {
+                            println!("At {} Request {} was fulfilled", self.clock.now, r_id);
+                        }
+                    }
+                    else {
+                        req.execution_time -= self.clock.step as u64;
+                    }
+                }
+            }
             self.clock.tick();
         }
     }
--- EP_2019/rust_impl/src/main.rs
+++ EP_2019/rust_impl/src/main.rs
@@ -8,9 +8,8 @@ mod passenger;
 mod car;
 
 fn main() {
-    //let clock = core::Clock::create("2019-07-06T00:00:00", "2019-07-06T00:00:05");
     let mut rng = SmallRng::from_rng(thread_rng()).unwrap();
-    let mut clock = core::Clock{step: 1, unit: "s".to_string(), start: "2019-07-06T00:00:00".to_string(), end: "2019-07-06T00:02:00".to_string(), now: 0, number_ticks: 120};
+    let clock = core::Clock::new("2019-07-06T00:00:00+00:00", "2019-07-07T00:00:00+00:00");
     let mut world = core::World{clock: clock, x: 100, y: 100, cars: HashMap::new(), passengers: HashMap::new(), requests: HashMap::new()};
     for i in 1..201 {
         world.register_car(car::Car{id: i as u32, pos: (rng.gen_range(0, 100), rng.gen_range(0, 100)), free: true});
@@ -18,6 +17,5 @@ fn main() {
     for i in 1..2001 {
         world.register_passenger(passenger::Passenger{id: i, awaiting: false, position: (rng.gen_range(0, 100), rng.gen_range(0, 100))});
     }
-
-    world.run();
+    world.run(false);
 }
